0ce371550764b11a390abfb3d7d6691357ad18b3
Manjog Singh <manjogsingh@gmail.com>
2018-02-08 17:22:52 +0530

    Added: Data Fetching Script

---
+++ Assets/Scripts/DataParser.cs
@@ -0,0 +1,21 @@
+﻿using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataParser : MonoBehaviour {
+
+	public string url = "https://dae71ed4.ngrok.io/annualrainfall";
+	//public string url = "https://api.myjson.com/bins/187sb1";
+	//public string url ="https://api.myjson.com/bins/k3fkx";
+	WWW www;
+	JsonData jsonObject;
+	IEnumerator Start () {
+		www = new WWW (url);
+		yield return www;
+		jsonObject = JsonMapper.ToObject (www.text);
+		//Debug.Log (jsonObject[0]["name"]);
+		//Debug.Log(jsonObject[0]["annual_rainfall"][0]["2000"]);
+		//Debug.Log(jsonObject[0]["annual_rainfall"][0]["New Delhi"]); aditya
+	}
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using LitJson;
+
+public class State{
+    public int[] array {get; set;}
+}
